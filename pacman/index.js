402fffc974f471a06780476ba3ba3df5aca5d1f9
Fernanda Marques <f3rnandamarques@gmail.com>
2025-01-03 21:55:45 -0300

    feat: player direction

--- pacman/index.js
+++ pacman/index.js
@@ -28,20 +28,37 @@ class Player {
         this.position = position
         this.velocity = velocity
         this.radius = 15
+        this.radians = 0.75
+        this.openRate = 0.12
+        this.rotation = 0
     }
 
     draw() {
+        c.save()
+        c.translate(this.position.x, this.position.y)
+        c.rotate(this.rotation)
+        c.translate(-this.position.x, -this.position.y)
         c.beginPath()
-        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.arc(this.position.x,
+            this.position.y,
+            this.radius,
+            this.radians,
+            Math.PI * 2 - this.radians)
+        c.lineTo(this.position.x, this.position.y)
         c.fillStyle = 'yellow'
         c.fill()
         c.closePath()
+        c.restore()
     }
 
     update() {
         this.draw()
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
+
+        if (this.radians < 0 || this.radians > 0.75) this.openRate = -this.openRate
+
+        this.radians += this.openRate
     }
 }
 
@@ -498,6 +515,11 @@ function animate() {
         }
     }
 
+    // win condition goes here
+    if (pellets.length === 0) {
+        cancelAnimationFrame(animationId)
+    }
+
     // power ups go
     for (let i = powerUps.length - 1; 0 <= i; i--) {
         const powerUp = powerUps[i]
@@ -653,6 +675,11 @@ function animate() {
             ghost.prevCollisions = []
         }
     })
+
+    if (player.velocity.x > 0) player.rotation = 0
+    else if (player.velocity.x < 0) player.rotation = Math.PI
+    else if (player.velocity.y > 0) player.rotation = Math.PI / 2
+    else if (player.velocity.y < 0) player.rotation = Math.PI * 1.5
 
 }
 
